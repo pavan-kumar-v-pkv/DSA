1c84f5ceae3ed0321f4366751c0bf5e8dd1e2f69
pkv-woodstock <pkvstarscream@gmail.com>
2025-06-12 00:41:28 +0530

    slight changes

--- basic maths/13-LCM.cpp
+++ basic maths/13-LCM.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    int GCD(int n1,int n2) {
+    int GCD2(int n1,int n2) {
         int largest = 1;
         int mini = min(n1, n2);
 
@@ -13,6 +13,18 @@ public:
                 return i;
         }
         return largest;
+    }
+    int GCD(int n1, int n2){
+        while(n1!=0 && n2 !=0){
+            if (n1 > n2)
+                n1 = n1 % n2;
+            else
+                n2 = n2 % n1;
+        }
+        if(n1 == 0)
+            return n2;
+        else 
+            return n1;
     }
     int LCM(int n1,int n2) {
         int gcd = GCD(n1, n2);
